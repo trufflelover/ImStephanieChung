4be2b6a4da6fb5879c0a4197c2000ae5da9e0641
trufflelover <stephaniechung1764@gmail.com>
2023-07-10 02:39:28 -0700

    good change

---
+++ first/dataprojects/js/init.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // This is a placeholder for any JavaScript functionality you might want to add
+    // For example, maybe you'd like to add some interaction with the map you mentioned:
+    /*
+    let myMap = L.map('maps').setView([51.505, -0.09], 13);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+    }).addTo(myMap);
+    */
+});
